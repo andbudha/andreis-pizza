76a5853b43a7f4443b3862727d5bdf331a768f32
andbudha <andreibartov@gmail.com>
2023-12-17 10:16:27 +0100

    Home comp created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css';
+import './App.scss';
+import { Home } from './components/Home/Home';
 
 function App() {
   return (
     <div className={'app-container'}>
-      <h1>Hi there!</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,6 @@
+import styles from './Home.module.scss';
+
+type Props = {};
+export const Home = (props: Props) => {
+  return <div className={styles.home_container}></div>;
+};
