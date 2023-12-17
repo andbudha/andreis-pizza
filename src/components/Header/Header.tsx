bd9b76d25f15f82b16627439688d58e92b55a45d
andbudha <andreibartov@gmail.com>
2023-12-17 11:31:36 +0100

    App updated

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,13 @@
 // @flow
 import styles from './Header.module.scss';
 import * as React from 'react';
+import { Logo } from './Logo/Logo';
 
 type Props = {};
 export const Header = (props: Props) => {
   return (
     <div className={styles.header_container}>
-      <div className={styles.logo_container}></div>
+      <Logo />
       <div className={styles.search_container}></div>
       <div className={styles.cart_info_container}></div>
     </div>
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,11 @@
+import { Header } from '../Header/Header';
 import styles from './Home.module.scss';
 
 type Props = {};
 export const Home = (props: Props) => {
-  return <div className={styles.home_container}></div>;
+  return (
+    <div className={styles.home_container}>
+      <Header />
+    </div>
+  );
 };
