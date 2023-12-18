c493de5f5b98f03971b0cf268f4178bce5756b13
andbudha <andreibartov@gmail.com>
2023-12-18 13:27:38 +0100

    Footer comp added and styled

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,24 @@
+import styles from './Foter.module.scss';
+
+type Props = {};
+export const Footer = (props: Props) => {
+  return (
+    <div className={styles.footer_container}>
+      <div className={styles.info_box}>
+        <div className={styles.total_price}>
+          <h3>
+            Total sum: <span>{38.5} €.</span>
+          </h3>
+        </div>
+        <div className={styles.total_items}>
+          <h3>
+            Items in cart: <span>{3} pcs.</span>
+          </h3>
+        </div>
+      </div>
+      <div className={styles.pagination_box}>
+        <h3>...123...</h3>
+      </div>
+    </div>
+  );
+};
