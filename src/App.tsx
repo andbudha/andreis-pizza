48d331a85939b40486c5935e76675dd31ded4313
andbudha <andreibartov@gmail.com>
2023-12-19 16:35:50 +0100

    App comp updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import { Home } from './components/Home/Home';
+import { Cart } from './components/Cart/Cart';
+import { Header } from './components/Header/Header';
 
 function App() {
   return (
-    <div className={'app-container'}>
-      <Home />
+    <div className={'app_container'}>
+      <div className={'wrapper'}>
+        <Header />
+        <div className={'app_content'}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
