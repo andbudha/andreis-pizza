f1aecaa87b38ed7a5df2f4bd6fc84e4d61003174
andbudha <andreibartov@gmail.com>
2023-12-19 16:37:11 +0100

    CartInfo comp updated

--- src/components/Header/CartInfo/CartInfo.tsx
+++ src/components/Header/CartInfo/CartInfo.tsx
@@ -1,15 +1,20 @@
+import { Link } from 'react-router-dom';
 import styles from './CartInfo.module.scss';
 import { BiCart } from 'react-icons/bi';
 type Props = {};
 export const CartInfo = (props: Props) => {
   return (
-    <div className={styles.cartinfo_container}>
-      <div className={styles.total_price_box}>{0} €</div>
-      <div className={styles.separation_box}></div>
-      <div className={styles.item_amount_box}>
-        <BiCart className={styles.cart_icon} color="white" />
-        <div className={styles.item_amount}>{0}</div>
-      </div>
-    </div>
+    <Link to={'/cart'} className={styles.link}>
+      <>
+        <div className={styles.cartinfo_container}>
+          <div className={styles.total_price_box}>{0} €</div>
+          <div className={styles.separation_box}></div>
+          <div className={styles.item_amount_box}>
+            <BiCart className={styles.cart_icon} color="white" />
+            <div className={styles.item_amount}>{0}</div>
+          </div>
+        </div>
+      </>
+    </Link>
   );
 };
