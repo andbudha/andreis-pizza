6af8bb9412832522a5e4a128019c10aea8bd078a
andbudha <andreibartov@gmail.com>
2023-12-20 22:24:17 +0100

    DAL created API tequests moved into new dir

---
+++ src/api/pizzaAPI.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { Pizza } from '../assets/types/types';
+
+export const instance = axios.create({
+  baseURL: 'https://656897589927836bd975198a.mockapi.io/reactpizza/api/1/items',
+});
+
+export const pizzaAPI = {
+  fetchPizzas: () => {
+    return instance.get<Pizza[]>('?&limit=6&page=1');
+  },
+};
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,35 +5,46 @@ import { PizzaCard } from '../PizzaCard/PizzaCard';
 import styles from './Home.module.scss';
 import axios from 'axios';
 import { Pizza } from '../../assets/types/types';
+import { AppRootState, useAppDispatch } from '../../redux/store';
+import { useSelector } from 'react-redux';
+import { pizzaSliceThunks } from '../../redux/slices/pizzaSlice';
+import { PizzaCardSkeleton } from '../PizzaCard/PizzaCardSkeleton/PizzaCardSkeleton';
 
 type Props = {};
 export const Home = (props: Props) => {
-  const [pizzas, setPizzas] = useState<Pizza[]>([]);
+  const dispatch = useAppDispatch();
+  const isLoading = useSelector<AppRootState, boolean>(
+    (state) => state.pizzas.isLoading
+  );
+  const pizzas = useSelector<AppRootState, Pizza[]>(
+    (state) => state.pizzas.pizzas
+  );
 
+  console.log(isLoading);
   useEffect(() => {
-    axios
-      .get<Pizza[]>(
-        'https://656897589927836bd975198a.mockapi.io/reactpizza/api/1/items?&limit=6&page=1'
-      )
-      .then((res) => {
-        setPizzas(res.data);
-      });
+    dispatch(pizzaSliceThunks.fetchPizzas());
   }, []);
 
   const pizzaList = pizzas.map((pizza) => {
     return (
-      <div key={pizza.id}>
-        <PizzaCard
-          category={pizza.category}
-          id={pizza.id}
-          imageUrl={pizza.imageUrl}
-          name={pizza.name}
-          price={pizza.price}
-          rating={pizza.rating}
-          sizes={pizza.sizes}
-          types={pizza.types}
-        />
-      </div>
+      <>
+        <div key={pizza.id}>
+          {isLoading ? (
+            <PizzaCardSkeleton key={pizza.id} />
+          ) : (
+            <PizzaCard
+              category={pizza.category}
+              id={pizza.id}
+              imageUrl={pizza.imageUrl}
+              name={pizza.name}
+              price={pizza.price}
+              rating={pizza.rating}
+              sizes={pizza.sizes}
+              types={pizza.types}
+            />
+          )}
+        </div>
+      </>
     );
   });
 
