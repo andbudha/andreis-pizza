c56e8350c7a9667dbab8e236e2a535149c74c224
andbudha <andreibartov@gmail.com>
2023-12-21 12:56:10 +0100

    cartSlice nuc-tx updated

--- src/redux/slices/cartSlice.tsx
+++ src/redux/slices/cartSlice.tsx
@@ -5,10 +5,60 @@ const slice = createSlice({
   name: 'cart',
   initialState: {
     cartItems: [] as CartPizza[],
+    totalItemAmount: 0 as number,
+    totalPrice: 0 as number,
   },
   reducers: {
     addPizza: (state, action: PayloadAction<{ addedPizza: CartPizza }>) => {
-      state.cartItems.unshift(action.payload.addedPizza);
+      let foundPizza = state.cartItems.find(
+        (pizza) => pizza.id === action.payload.addedPizza.id
+      );
+      if (!foundPizza) {
+        state.cartItems.unshift({ ...action.payload.addedPizza, count: 1 });
+      } else if (foundPizza && foundPizza.count) {
+        foundPizza.count++;
+      }
+
+      state.totalItemAmount = state.cartItems.reduce(
+        (amount, item) => (item.count ? amount + item.count : 0),
+        0
+      );
+      state.totalPrice = state.cartItems.reduce(
+        (amount, item) => (item.count ? amount + item.count * item.price : 0),
+        0
+      );
+    },
+    addSimilarPizza: (state, action: PayloadAction<{ id: string }>) => {
+      let foundItem = state.cartItems.find(
+        (pizza) => pizza.id === action.payload.id
+      );
+      if (foundItem && foundItem.count) {
+        foundItem.count++;
+      }
+      state.totalItemAmount = state.cartItems.reduce(
+        (amount, item) => (item.count ? amount + item.count : 0),
+        0
+      );
+      state.totalPrice = state.cartItems.reduce(
+        (amount, item) => (item.count ? amount + item.count * item.price : 0),
+        0
+      );
+    },
+    removeSimilarPizza: (state, action: PayloadAction<{ id: string }>) => {
+      let foundItem = state.cartItems.find(
+        (pizza) => pizza.id === action.payload.id
+      );
+      if (foundItem && foundItem.count) {
+        foundItem.count--;
+      }
+      state.totalItemAmount = state.cartItems.reduce(
+        (amount, item) => (item.count ? amount + item.count : 0),
+        0
+      );
+      state.totalPrice = state.cartItems.reduce(
+        (amount, item) => (item.count ? amount + item.count * item.price : 0),
+        0
+      );
     },
     emptyCart: (state, action: PayloadAction<[]>) => {
       state.cartItems = action.payload;
@@ -16,6 +66,14 @@ const slice = createSlice({
     removePizza: (state, action: PayloadAction<{ id: string }>) => {
       state.cartItems = state.cartItems.filter(
         (item) => item.id !== action.payload.id
+      );
+      state.totalItemAmount = state.cartItems.reduce(
+        (amount, item) => (item.count ? amount + item.count : 0),
+        0
+      );
+      state.totalPrice = state.cartItems.reduce(
+        (amount, item) => (item.count ? amount + item.count * item.price : 0),
+        0
       );
     },
   },
